bfb765649948709af47e2d99f37bfe793dbf05fb
thiri <thiriaung.java.it@gmail.com>
2025-11-13 22:08:40 +0630

    creating blogs and blog

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //  return view('welcome'); //ဒါက သူ့ default ခေါ်တဲ့ဟာ
+    // return "Creative Coder";
+    // return ["key"=>"Creative Coder"]; ဆိုလိုတာက view file ကိုပဲ return ပြန်လို့ရတာမဟုတ်ဘူး ဘာကိုထည့်ထည့် သူကပြောင်းပေါ်ပေးမှာ
+    return view('blogs');
 });
